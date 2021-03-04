3c7434fe1c44d971e29b8f2d34aef703d305bc88
Nikita Buzov <nikitabuzov@gmail.com>
2021-03-03 23:15:47 -0800

    SOLACE _mint tests pass

--- test/SOLACE-test.js
+++ test/SOLACE-test.js
@@ -1,41 +1,57 @@
-// import {expect, use} from 'chai';
 const { expect, use } = require('chai');
-const {Contract} = require('ethers');
-const {deployContract, MockProvider, solidity} = require('ethereum-waffle');
+const { waffle } = require("hardhat");
+const { deployContract, solidity } = waffle;
+const provider = waffle.provider;
 const SOLACE = require('../artifacts/contracts/SOLACE.sol/SOLACE.json');
 
 use(solidity);
 
 describe('SolaceToken', () => {
-  const [wallet, walletTo] = new MockProvider().getWallets();
-  // let token: Contract;
+  const [wallet, walletTo, anotherAccount] = provider.getWallets();
+
+  const name = 'solace.fi';
+  const symbol = 'SOLACE';
+  ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
   beforeEach(async () => {
-    token = await deployContract(wallet, SOLACE, [1000]);
+    token = await deployContract(wallet, SOLACE);
   });
 
-  it('Assigns initial balance', async () => {
-    expect(await token.balanceOf(wallet.address)).to.equal(1000);
+  it('has a correct name', async function () {
+    expect(await token.name()).to.equal(name);
   });
 
-  it('Transfer emits event', async () => {
-    await expect(token.transfer(walletTo.address, 7))
-      .to.emit(token, 'Transfer')
-      .withArgs(wallet.address, walletTo.address, 7);
+  it('has a correct symbol', async function () {
+    expect(await token.symbol()).to.equal(symbol);
   });
 
-  it('Can not transfer above the amount', async () => {
-    await expect(token.transfer(walletTo.address, 1007))
-      .to.be.revertedWith('VM Exception while processing transaction: revert ERC20: transfer amount exceeds balance');
+  it('has 18 decimals', async function () {
+    expect(await token.decimals()).to.equal(18);
   });
 
-  it('Send transaction changes receiver balance', async () => {
-    await expect(() => wallet.sendTransaction({to: walletTo.address, gasPrice: 0, value: 200}))
-      .to.changeBalance(walletTo, 200);
-  });
+  describe('_mint', function () {
+    const amount = 50;
+    it('rejects a null account', async function () {
+      await expect(token.mint(ZERO_ADDRESS, amount),).to.be.reverted;
+    });
 
-  it('Send transaction changes sender and receiver balances', async () => {
-    await expect(() =>  wallet.sendTransaction({to: walletTo.address, gasPrice: 0, value: 200}))
-      .to.changeBalances([wallet, walletTo], [-200, 200]);
+    describe('for a non zero account', function () {
+      beforeEach('minting', async function () {
+        await token.addMinter(wallet.address)
+        await token.mint(wallet.address, amount);
+      });
+
+      it('increments totalSupply', async function () {
+        expect(await token.totalSupply()).to.equal(amount);
+      });
+
+      it('increments recipient balance', async function () {
+        expect(await token.balanceOf(wallet.address)).to.equal(amount);
+      });
+
+      it('emits Transfer event', async function () {
+        expect(await token.mint(wallet.address, amount)).to.emit(token, 'Transfer').withArgs(ZERO_ADDRESS, wallet.address, amount);
+      });
+    });
   });
 });
