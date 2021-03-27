3495821bb8aaeb2e9cebe93797a1062c184732fd
Nikita Buzov <nikitabuzov@gmail.com>
2021-03-27 18:41:22 -0500

    feat(Product): updates to BaseProduct and IProduct

--- contracts/BaseProduct.sol
+++ contracts/BaseProduct.sol
@@ -1,40 +1,54 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Address.sol";
-
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import './interfaces/IProduct.sol';
+import './Policy.sol';
 /**
  * @title BaseProduct
  * @author solace.fi
  * @notice To be inherited by individual Product contracts.
  */
-abstract contract BaseProduct {
+abstract contract BaseProduct is IProduct {
     using Address for address;
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    event PolicyCreated(uint256 _coverLimit, uint256 _days, uint256 positionAmount, uint256 premium, address policy, uint256 policies.length, uint256 coveredAmount)
+
+    // Governor
     address public governance;
 
-    // Individual Product Details
-    string public productName; // Should we store as an int and hold conversion to name in registry. bytes32 instead of string?
-    uint256 public pricePerCoveredEther; // cover price(in wei) per ether
-    uint256 public cancelReimbursementPerMonthRemaining; // reimbursment price(in wei) per month remaining on policy
-    uint256 public minPeriodMonths; // minimum period in months for a Policy
-    uint256 public maxPeriodMonths; // maximum period in months for a Policy
-    
+    // Product Details
+    address public insuredContract;
+    uint256 public price; // cover price (in wei) per day per wei
+    uint256 public cancelFee; // policy cancelation fee
+    uint256 public minPeriod; // minimum policy period in days
+    uint256 public maxPeriod; // maximum policy period in days
+    uint256 public maxCoverAmount; // maximum amount of coverage (in wei) this product can sell
+
+    // Book-keeping varaibles
+    uint256 public policyCount = 0;
+    uint256 public coveredAmount; // current amount covered (in wei)
+    EnumerableSet.AddressSet private policies;
+    mapping(address => address) public buyerOf; // buyerOf[policy] = buyer
 
 
     constructor (
-        string _productName,
-        uint256 _minPeriodMonths,
-        uint256 _maxPeriodMonths,
-        uint256 _pricePerCoveredEther,
-        uint256 _cancelReimbursementPerMonthRemaining
+        address _insuredContract,
+        uint256 _price,
+        uint256 _cancelFee,
+        uint256 _minPeriod,
+        uint256 _maxPeriod,
+        uint256 _maxCoverAmount
     ) {
         governance = msg.sender;
-        productName = _productName;
-        minPeriodMonths = _minPeriodMonths;
-        maxPeriodMonths = _maxPeriodMonths;
-        pricePerCoveredEther = _pricePerCoveredEther;
-        cancelReimbursementPerMonthRemaining = _cancelReimbursementPerMonthRemaining;
+        insuredContract = _insuredContract;
+        price = _price;
+        cancelFee = _cancelFee;
+        minPeriod = _minPeriod;
+        maxPeriod = _maxPeriod;
+        maxCoverAmount = _maxCoverAmount
     }
 
     /**** GETTERS + SETTERS 
@@ -42,40 +56,48 @@ abstract contract BaseProduct {
     ****/
 
     /**
-     * @notice Sets the price per covered eth for this product
-     * @param _pricePerCoveredEther Price per covered eth in wei
+     * @notice Sets the price for this product
+     * @param _price cover price (in wei) per ether per day
      */
-    function setPricePerCoveredEther(uint256 _pricePerCoveredEther) external {
+    function setPrice(uint256 _price) external {
         require(msg.sender == governance, "!governance");
-        pricePerCoveredEther = _pricePerCoveredEther
+        price = _price
     }
 
     /**
-     * @notice Sets the cancellation reimbursement per month remaining unit
-     * @param _cancelReimbursementPerMonthRemaining Cancel reimbursement in wei per month 
-       remaining in policy
+     * @notice Sets the fee that user must pay upon canceling the policy
+     * @param _cancelFee policy cancelation fee 
      */
-    function setCancelReimbursementPerMonth(uint256 _cancelReimbursementPerMonthRemaining) external {
+    function setCancelFee(uint256 _cancelFee) external {
         require(msg.sender == governance, "!governance");
-        cancelReimbursementPerMonthRemaining = _cancelReimbursementPerMonthRemaining
+        cancelFee = _cancelFee
     }
 
     /**
-     * @notice Sets the minimum number of months a policy can be bought for
-     * @param _minPeriodMonths minimum number of months
+     * @notice Sets the minimum number of days a policy can be bought for
+     * @param _minPeriod minimum number of days
      */
-    function setMinPeriodMonths(uint256 _minPeriodMonths) external {
+    function setMinPeriod(uint256 _minPeriod) external {
         require(msg.sender == governance, "!governance");
-        minPeriodMonths = _minPeriodMonths
+        minPeriod = _minPeriod
     }
 
     /**
-     * @notice Sets the maximum number of months a policy can be bought for
-     * @param _maxPeriodMonths maximum number of months
+     * @notice Sets the maximum number of days a policy can be bought for
+     * @param _maxPeriod maximum number of days
      */
-    function setMaxPeriodMonths(uint256 _maxPeriodMonths) external {
+    function setMaxPeriod(uint256 _maxPeriod) external {
         require(msg.sender == governance, "!governance");
-        maxPeriodMonths = _maxPeriodMonths
+        maxPeriod = _maxPeriod
+    }
+
+    /**
+     * @notice Sets the maximum coverage amount this product can sell
+     * @param _maxCoverAmount maximum coverage amount (in wei)
+     */
+    function setMaxCoverAmount(uint256 _maxCoverAmount) external {
+        require(msg.sender == governance, "!governance");
+        maxCoverAmount = _maxCoverAmount
     }
 
     /**** UNIMPLEMENTED FUNCTIONS 
@@ -92,7 +114,7 @@ abstract contract BaseProduct {
      *  only be implemented in the inheriting product contracts
      * @return The user's total position in wei in the product's protocol.
      */
-    function getTotalPositionInProduct() virtual public view returns (uint256);
+    function getTotalPosition(address _buyer) virtual public view returns (uint256 positionAmount){};
 
 
     /**** METRIC VIEW FUNCTIONS 
@@ -100,17 +122,24 @@ abstract contract BaseProduct {
     ****/
 
     /**
-     * @notice
-     *  Get the total covered amount for all policies under this product
+     * @notice Get the total covered amount for all policies under this product
      * @return Amount of total cover denominated in wei
      */
-    function getTotalCover() external view returns (uint256){
-
+    function getTotalCovered() external view returns (uint256 coveredAmount){
+        /* Todo update coveredAmount: iterate through each policy and call getTotalPosition(buyerOf[policy]) */
+        return coveredAmount
     }
 
     /*** POLICY VIEW FUNCTIONS 
     View functions that give us data about individual policies
     ****/
+
+    function getPolicyLimit(address _policy) public view returns (uint256 coverLimit){};
+
+    // function getPolicyCoverAmount(address _policy) public view returns (uint256 coverAmount){};
+
+    function getPolicyExpiration(address _policy) public view returns (uint256 expirationDate){};
+ 
 
     /**** QUOTE VIEW FUNCTIONS 
     View functions that give us quotes regarding a policy
@@ -119,79 +148,62 @@ abstract contract BaseProduct {
     /**
      * @notice
      *  Provide a premium quote.
-     * @param coverPercentage percentage of cover for total position
-     * @param months length (in months) of policy
+     * @param _coverLimit percentage of cover for total position
+     * @param _days length (in days) for policy
      * @return The quote for their policy in wei.
      */
-    function getQuote(uint256 coverPercentage, uint256 months) external view returns (uint256){
-        /* Todo(kush): Implement getQuote
-         1. Pass inputs to oracle which creates a quote by talking to offchain system
-         2. Return quote from offchain system
-        */
+    function _getQuote(uint256 _coverLimit, uint256 _days, uint256 _positionAmount) internal view returns (uint256 premium){
+        require(_days > minPeriod && _days < maxPeriod, 'invalid period');
+        require(_coverLimit > 0 && _coverLimit < 100, 'invalid cover limit');
+        premium = _positionAmount * _coverLimit * _days * price;
+        return premium
     }
 
-    /**
-     * @notice
-     *  Provide an increased cover quote given the policy.
-     * @param policy address of existing policy
-     * @param coverPercentage new cover percentage
-     * @return The quote in wei for increasing the cover limit of the policy
-     */
-    function getIncreasedCoverQuote(address policy, uint256 coverPercentage) external view returns (uint256){
-        /* Todo(kush): Implement getIncreasedCoverQuote*/
+    function getQuote(uint256 _coverLimit, uint256 _days) external view returns (uint256 premium){
+        positionAmount = getTotalPosition(msg.sender);
+        return _getQuote(_coverPercentage, _days, positionAmount)
     }
-
-    /**
-     * @notice
-     *  Provide an extension quote given the policy.
-     * @param policy address of existing policy
-     * @param months length (in months) of extension
-     * @return The quote in wei for extending the policy by the inputted months
-     */
-    function getExtensionQuote(address policy, uint256 months) external view returns (uint256){
-        /* Todo(kush): Implement getExtensionQuote*/
-    }
-
-    /**
-     * @notice
-     *  Provide a cancellation reimbursement for the current plicy
-     * @param policy address of existing policy
-     * @return The amount in wei reimbursed for canceling the policy
-     */
-    function getCancelReimbursement(address policy) external view returns (uint256){
-        //Todo(kush): Implement cancelPolicy
-    }
-
 
 
     /**** MUTATIVE FUNCTIONS 
     Functions that deploy and change policy contracts
     ****/
 
-
     /**
      * @notice
      *  Purchase and deploy a policy on the behalf of msg.sender
-     * @param coverPercentage percentage of cover for total position
-     * @param months length (in months) of policy
+     * @param _coverLimit percentage of cover for total position
+     * @param _days length (in days) for policy
      * @return The contract address of the policy
      */
-    function buyPolicy(uint256 coverPercentage, uint256 months) external payable returns (address){
-        /*Todo(kush): Implement buyPolicy
-          1. Check getQuote matches payment
-          2. Check details of policy
-          3. Deploy policy contract
-        */
+    function buyPolicy(uint256 _coverLimit, uint256 _days) external payable returns (address policy){
+        positionAmount = getTotalPosition(msg.sender);
+        premium = _getQuote(_coverLimit, _days, positionAmount);
+        require(msg.value == premium, 'payment does not match the quote');
+        coveredAmount = getTotalCovered();
+        newCoverAmount = coveredAmount + _coverLimit * positionAmount;
+        require(newCoverAmount <= maxCoverAmount, 'max covered amount is reached');
+        bytes memory bytecode = type(Policy).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(_coverLimit, _days));
+        assembly {
+            policy := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        IPolicy(policy).initialize();
+        coveredAmount = newCoverAmount
+        buyerOf[policy] = msg.sender;
+        policies.add(policy);
+        emit PolicyCreated(_coverLimit, _days, positionAmount, premium, policy, policies.length, coveredAmount);
+        return policy
     }
 
     /**
      * @notice
-     *  Increase the cover limit for the policy
-     * @param policy address of existing policy
-     * @param coverPercentage new cover percentage
+     *  Increase or decrease the cover limit for the policy
+     * @param _policy address of existing policy
+     * @param _coverLimit new cover percentage
      * @return True if coverlimit is successfully increased else False
      */
-    function increasedCoverForPolicy(address policy, uint256 coverPercentage) external payable returns (bool){
+    function updateCoverLimit(address _policy, uint256 _coverLimit) external payable returns (bool){
         /* Todo(kush): Implement getIncreasedCoverQuote*/
     }
 
@@ -199,10 +211,10 @@ abstract contract BaseProduct {
      * @notice
      *  Extend a policy contract
      * @param policy address of existing policy
-     * @param months length (in months) of extension
+     * @param _days length (in months) of extension
      * @return True if successfully extended else False
      */
-    function extendPolicy(address policy, uint256 months) external payable returns (bool){
+    function extendPolicy(address policy, uint256 _days) external payable returns (bool){
         //Todo(kush): Implement extendPolicy
     }
 
@@ -215,6 +227,4 @@ abstract contract BaseProduct {
     function cancelPolicy(address policy) external returns (bool){
         //Todo(kush): Implement cancelPolicy
     }
-
-
 }
