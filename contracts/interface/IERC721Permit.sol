3aeb2e31cd9f81f4bd13d8a999f92bdfc5fae0c2
Andrew Leonard <leonardishere@gmail.com>
2021-07-30 08:56:17 -0700

    feat(Solidity): bump pragma 0.8.0 -> 0.8.6

--- contracts/interface/IERC721Permit.sol
+++ contracts/interface/IERC721Permit.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // code borrowed from @uniswap/v3-periphery
-pragma solidity 0.8.0;
+pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
--- contracts/interface/IWETH9.sol
+++ contracts/interface/IWETH9.sol
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity 0.8.0;
+pragma solidity 0.8.6;
 
 interface IWETH9 {
     function name() external view returns (string memory);
