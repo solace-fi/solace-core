d87282920a35e93ffe7db4cf22f8476fe90e5229
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2021-12-08 21:22:22 +1100

    Renamed InvestorVesting.sol to TokenVesting.sol, implemented Github PR comments

--- contracts/TokenVesting.sol
+++ contracts/TokenVesting.sol
@@ -1,5 +1,3 @@
-// UNSURE - Are we setting the vesting period to start retrospective to this contract's deployment?
-
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.6;
 
@@ -10,14 +8,14 @@ import "./Governable.sol";
 import "./interface/ISOLACE.sol";
 
 /**
- * @title InvestorVesting
+ * @title TokenVesting
  * @author solace.fi
  * @notice Stores and handles vested [**SOLACE**](./SOLACE) tokens for SOLACE investors
  *
  * Predetermined agreement with investors for a linear unlock over three years, with a six month cliff
  */
 
- contract InvestorVesting is ReentrancyGuard, Governable {
+ contract TokenVesting is ReentrancyGuard, Governable {
  
     /// @notice SOLACE Token.
     address public solace;
@@ -40,10 +38,6 @@ import "./interface/ISOLACE.sol";
     /// @notice Redeemed tokens for an investor.
     mapping(address => uint256) public redeemedInvestorTokens;
 
-    // @dev used in our floating point price maths
-    // Specifically where we determine unlocked proportion of investor tokens
-    uint256 internal constant Q12 = 1e12;
-
     /**
      * @notice Constructs the `InvestorVesting` contract.
      * @param governance_ The address of the [governor](/docs/protocol/governance).
@@ -63,38 +57,18 @@ import "./interface/ISOLACE.sol";
     }
 
     /***************************************
-    VIEW FUNCTIONS
-    ***************************************/
-
-    /// @notice View total SOLACE token amount for an investor address.
-    function viewTotalTokens(address investor) external view returns (uint256) {
-        return totalInvestorTokens[investor];
-    }
-
-    /// @notice View redeemed SOLACE token amount for an investor address.
-    function viewRedeemedTokens(address investor) external view returns (uint256) {
-        return redeemedInvestorTokens[investor];
-    }
-
-    /// @notice View unlocked SOLACE token amount that an investor can claim 
-    function viewRedeemableUnlockedTokens(address investor) external view returns (uint256) {
-        uint256 redeemableUnlockedAmount = _getRedeemableUnlockedTokens(investor);
-        return redeemableUnlockedAmount;
-    }
-
-    /***************************************
     INVESTOR FUNCTIONS
     ***************************************/
 
     /**
-     * @notice Function for investor to claim SOLACE tokens
-     * @param amount Amount of SOLACE tokens to claim from this contract
+     * @notice Function for investor to claim SOLACE tokens - will claim all redeemable tokens
      */
-    function claimTokens (uint256 amount) external nonReentrant {
+    function claimTokens () external nonReentrant {
         require(totalInvestorTokens[msg.sender] != 0, "You have no tokens to claim");
-        require(amount <= _getRedeemableUnlockedTokens(msg.sender), "You cannot claim this many tokens");
-        redeemedInvestorTokens[msg.sender] += amount;
-        SafeERC20.safeTransfer(IERC20(solace), msg.sender, amount);
+        require(getRedeemableUnlockedTokens(msg.sender) > 0, "You cannot claim this many tokens");
+        uint256 redeemableUnlockedTokens = getRedeemableUnlockedTokens(msg.sender);
+        redeemedInvestorTokens[msg.sender] += redeemableUnlockedTokens;
+        SafeERC20.safeTransfer(IERC20(solace), msg.sender, redeemableUnlockedTokens);
     }
 
     /**
@@ -102,17 +76,14 @@ import "./interface/ISOLACE.sol";
      * @param investor Investor address
      * @return redeemableUnlockedAmount The amount of unlocked tokens an investor can claim from the smart contract
      */
-    function _getRedeemableUnlockedTokens(address investor) internal view returns (uint256 redeemableUnlockedAmount) {
+    function getRedeemableUnlockedTokens(address investor) public view returns (uint256 redeemableUnlockedAmount) {
         uint256 timestamp = block.timestamp;
         uint256 redeemableUnlockedAmount;
         if(timestamp <= cliff) {
-            return redeemableUnlockedAmount; // Return default value of 0
+            return 0;
         } else if(timestamp <= vestingEnd) {
-            uint256 timeSinceCliff = timestamp - cliff;
-            uint256 timeBetweenVestingEndAndCliff = vestingEnd - cliff;
-            uint256 totalUnlockedProportion = Q12 * (timeSinceCliff / timeBetweenVestingEndAndCliff); // Solidity rounds down to 0, so use fixed point arithmetic here
-            uint256 totalUnlockedInvestorTokens = (totalUnlockedProportion * totalInvestorTokens[investor]) / Q12;
-            redeemableUnlockedAmount = totalUnlockedInvestorTokens - redeemedInvestorTokens[investor];
+            uint256 totalUnlockedAmount = totalInvestorTokens[investor] * ( (timestamp - vestingStart) / (vestingEnd - vestingStart) );
+            redeemableUnlockedAmount = totalUnlockedAmount - redeemedInvestorTokens[investor];
             return redeemableUnlockedAmount;
         } else {
             redeemableUnlockedAmount = totalInvestorTokens[investor] - redeemedInvestorTokens[investor];
@@ -138,17 +109,17 @@ import "./interface/ISOLACE.sol";
     /**
      * @notice Rescues excess [**SOLACE**](./SOLACE).
      * Can only be called by the current [**governor**](/docs/protocol/governance).
+     * @dev Trusting governance to perform accurate accounting off-chain and ensure there is sufficient SOLACE in contract to make payouts as dictated in totalInvestorTokens mapping
      * @param amount Amount to send. Will be sent from this contract to `custodian`.
      */
     function rescueSOLACEtokens(uint256 amount) external onlyGovernance {
         SafeERC20.safeTransfer(IERC20(solace), custodian, amount);
-        // UNSURE - If there is any non-zero value in the totalInvestorTokens mapping, the mapping will become uncorrelated with the actual amount of SOLACE tokens held by the contract
-        // Do we want to implement a guard to avoid the above messup?
     }
 
     /**
      * @notice Sets the total SOLACE token amounts that investors are eligible for.
      * Can only be called by the current [**governor**](/docs/protocol/governance).
+     * @dev Trusting governance to perform accurate accounting off-chain and ensure there is sufficient SOLACE in contract to make payouts as dictated in totalInvestorTokens mapping
      * @param investors Array of investors to set.
      * @param totalTokenAmounts Array of token amounts to set.
      */
@@ -156,7 +127,7 @@ import "./interface/ISOLACE.sol";
         require(investors.length == totalTokenAmounts.length, "length mismatch");
         for(uint256 i = 0; i < investors.length; i++) {
             totalInvestorTokens[investors[i]] = totalTokenAmounts[i];
-            SafeERC20.safeTransfer(IERC20(solace), custodian, totalTokenAmounts[i]); // UNSURE here - Where are we transferring SOLACE tokens from? Also do we care about lost gas efficiency in transferring SOLACE in multiple transactions within each loop iteration vs in one single transaction elsewhere?
         }
+        // UNSURE - should we use a checksum to ensure (sum of []totalTokenAmounts) <= (SOLACE balance of this contract)), or trust governance
     }
  }
