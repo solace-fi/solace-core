989d565b97746a48e484c6e911cdd79fb488769f
Andrew Leonard <leonardishere@gmail.com>
2022-03-08 00:03:26 -0800

    deploy on Polygon

--- scripts/deploy-polygon-4.ts
+++ scripts/deploy-polygon-4.ts
@@ -28,7 +28,7 @@ const PREMIUM_COLLECTOR_ADDRESS               = "0xF321be3577B1AcB436869493862bA
 const REGISTRY_V2_ADDRESS               = "0x501ACe0f576fc4ef9C0380AA46A578eA96b85776";
 const RISK_MANAGER_V2_ADDRESS           = "0x501AcEf9020632a71CB25CFa9F554252eB51732b";
 const COVERAGE_DATA_PROVIDER_ADDRESS    = "0x501ACE6C5fFf4d42EaC02357B6DD9b756E337355";
-const SOLACE_COVER_PRODUCT_ADDRESS      = "0x501ACed5c34d01Db4da47d18c0411936a6a022dE";
+const SOLACE_COVER_PRODUCT_ADDRESS      = "0x501AcEC83d440c00644cA5C48d059e1840852a64";
 
 let artifacts: ArtifactImports;
 let deployerContract: Deployer;
@@ -39,6 +39,11 @@ let riskManagerV2: RiskManager;
 let solaceCoverProduct: SolaceCoverProductV2;
 
 let signerAddress: string;
+
+const ONE_GWEI = 1000000000;
+const maxFeePerGas = 61 * ONE_GWEI;
+const maxPriorityFeePerGas = 31 * ONE_GWEI;
+
 async function main() {
   artifacts = await import_artifacts();
   signerAddress = await deployer.getAddress();
@@ -56,7 +61,6 @@ async function main() {
 
   // deploy contracts
   await deployRegistry()
-
   await deployCoverageDataProvider();
   await deployRiskManager();
   await deploySolaceCoverProductV2();
@@ -74,16 +78,17 @@ async function deployRegistry() {
     registryV2 = (await ethers.getContractAt(artifacts.Registry.abi, res.address)) as Registry;
     console.log(`Deployed Registry(V2) to ${registryV2.address}`);
   }
-
+  /*
   // set default addresses
   if (await registryV2.governance() == signerAddress) {
     console.log("Setting 'FRAX', 'premiumPool', 'coverPromotionAdmin', 'premiumCollector', 'riskManager', 'coverageDataProvider', 'solaceCoverProduct' addresses");
     let tx = await registryV2.connect(deployer).set(
           ["frax", "premiumPool", "coverPromotionAdmin", "premiumCollector", "riskManager", "coverageDataProvider", "solaceCoverProduct"],
           [FRAX_ADDRESS, PREMIUM_POOL_ADDRESS, COVER_PROMOTION_ADMIN_ADDRESS, PREMIUM_COLLECTOR_ADDRESS, RISK_MANAGER_V2_ADDRESS, COVERAGE_DATA_PROVIDER_ADDRESS, SOLACE_COVER_PRODUCT_ADDRESS]
-        );
+        , {maxFeePerGas, maxPriorityFeePerGas});
     await tx.wait()
   }
+  */
 }
 
 async function deployCoverageDataProvider() {
@@ -99,28 +104,20 @@ async function deployCoverageDataProvider() {
   const { success } = await registryV2.tryGet("coverageDataProvider");
   if (!success && await registryV2.governance() == signerAddress) {
     console.log("Registering Coverage Data Provider");
-    let tx = await registryV2.connect(deployer).set(["coverageDataProvider"], [coverageDataProvider.address]);
+    let tx = await registryV2.connect(deployer).set(["coverageDataProvider"], [coverageDataProvider.address], {maxFeePerGas, maxPriorityFeePerGas});
     await tx.wait();
 
     console.log("Setting Underwriting Pool Updater");
-    tx = await coverageDataProvider.connect(deployer).setUwpUpdater(COVERAGE_DATA_PROVIDER_UPDATER_ADDRESS);
+    tx = await coverageDataProvider.connect(deployer).setUwpUpdater(COVERAGE_DATA_PROVIDER_UPDATER_ADDRESS, {maxFeePerGas, maxPriorityFeePerGas});
     await tx.wait();
 
     console.log("Setting Underwriting Pool Amounts");
-    tx = await coverageDataProvider.connect(deployer).set("mainnet", BN.from("1000000000000000000").mul(8450000)); // 8.45M USD
+    tx = await coverageDataProvider.connect(deployer).set("mainnet", BN.from("1000000000000000000").mul(8450000), {maxFeePerGas, maxPriorityFeePerGas}); // 8.45M USD
     await tx.wait();
 
     // tx = await coverageDataProvider.connect(deployer).set("aurora", AMOUNT2);
     // await tx.wait();
   }
-
-  console.log("Setting Underwriting Pool Updater");
-  let tx = await coverageDataProvider.connect(deployer).setUwpUpdater(COVERAGE_DATA_PROVIDER_UPDATER_ADDRESS);
-  await tx.wait();
-
-  console.log("Setting Underwriting Pool Amounts");
-  tx = await coverageDataProvider.connect(deployer).set("mainnet", BN.from("1000000000000000000").mul(8450000)); // 8.45M USD
-  await tx.wait();
 }
 
 async function deployRiskManager() {
@@ -136,7 +133,7 @@ async function deployRiskManager() {
   const { success } = await registryV2.tryGet("riskManager");
   if (!success && await registryV2.governance() == signerAddress) {
     console.log("Registering Risk Manager");
-    let tx = await registryV2.connect(deployer).set(["riskManager"], [riskManagerV2.address]);
+    let tx = await registryV2.connect(deployer).set(["riskManager"], [riskManagerV2.address], {maxFeePerGas, maxPriorityFeePerGas});
     await tx.wait();
   }
 }
@@ -157,49 +154,49 @@ async function deploySolaceCoverProductV2() {
   let { success, value } = await registryV2.tryGet("solaceCoverProduct");
   if ((!success || value != solaceCoverProduct.address) && await registryV2.governance() == signerAddress) {
     console.log("Registering Solace Cover Product");
-    let tx = await registryV2.connect(deployer).set(["solaceCoverProduct"], [solaceCoverProduct.address]);
+    let tx = await registryV2.connect(deployer).set(["solaceCoverProduct"], [solaceCoverProduct.address], {maxFeePerGas, maxPriorityFeePerGas});
     await tx.wait();
   }
 
   ({ success } = await registryV2.tryGet("coverPromotionAdmin"));
   if ( !success && await registryV2.governance() == signerAddress) {
     console.log("Registering Cover Promotion Admin");
-    let tx = await registryV2.connect(deployer).set(["coverPromotionAdmin"], [COVER_PROMOTION_ADMIN_ADDRESS]);
+    let tx = await registryV2.connect(deployer).set(["coverPromotionAdmin"], [COVER_PROMOTION_ADMIN_ADDRESS], {maxFeePerGas, maxPriorityFeePerGas});
     await tx.wait();
   }
 
   ({ success } = await registryV2.tryGet("premiumPool"));
   if ( !success && await registryV2.governance() == signerAddress) {
     console.log("Registering Premium Pool");
-    let tx = await registryV2.connect(deployer).set(["premiumPool"], [PREMIUM_POOL_ADDRESS]);
+    let tx = await registryV2.connect(deployer).set(["premiumPool"], [PREMIUM_POOL_ADDRESS], {maxFeePerGas, maxPriorityFeePerGas});
     await tx.wait();
   }
 
   ({ success } = await registryV2.tryGet("premiumCollector"));
   if ( !success && await registryV2.governance() == signerAddress) {
     console.log("Registering Premium Collector");
-    let tx = await registryV2.connect(deployer).set(["premiumCollector"], [PREMIUM_COLLECTOR_ADDRESS]);
+    let tx = await registryV2.connect(deployer).set(["premiumCollector"], [PREMIUM_COLLECTOR_ADDRESS], {maxFeePerGas, maxPriorityFeePerGas});
     await tx.wait();
   }
 
   console.log('Risk Manager(V2) - Adding Soteria as Risk Strategy');
-  let tx = await riskManagerV2.connect(deployer).addRiskStrategy(solaceCoverProduct.address)
+  let tx = await riskManagerV2.connect(deployer).addRiskStrategy(solaceCoverProduct.address, {maxFeePerGas, maxPriorityFeePerGas})
   await tx.wait();
 
   console.log('Risk Manager(V2) - Setting Soteria as an active strategy');
-  tx = await riskManagerV2.connect(deployer).setStrategyStatus(solaceCoverProduct.address, 1)
+  tx = await riskManagerV2.connect(deployer).setStrategyStatus(solaceCoverProduct.address, 1, {maxFeePerGas, maxPriorityFeePerGas})
   await tx.wait();
 
   console.log('Risk Manager(V2) - Setting Soteria weight allocation');
-  tx = await riskManagerV2.connect(deployer).setWeightAllocation(solaceCoverProduct.address, 1000)
+  tx = await riskManagerV2.connect(deployer).setWeightAllocation(solaceCoverProduct.address, 1000, {maxFeePerGas, maxPriorityFeePerGas})
   await tx.wait();
 
   console.log('Risk Manager(V2) - Adding Soteria as a cover limit updated');
-  tx = await riskManagerV2.connect(deployer).addCoverLimitUpdater(solaceCoverProduct.address)
+  tx = await riskManagerV2.connect(deployer).addCoverLimitUpdater(solaceCoverProduct.address, {maxFeePerGas, maxPriorityFeePerGas})
   await tx.wait();
 
   console.log('Adding supported chains to cover product');
-  let tx2 = await solaceCoverProduct.connect(deployer).addSupportedChains([137]);
+  let tx2 = await solaceCoverProduct.connect(deployer).addSupportedChains([137], {maxFeePerGas, maxPriorityFeePerGas});
   await tx2.wait();
 
 }
