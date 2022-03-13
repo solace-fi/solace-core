5f2183e7643662e3acc3fcd50b1dbce236ec0e07
Andrew Leonard <leonardishere@gmail.com>
2022-03-12 23:22:01 -0800

    create solace cover minutes

---
+++ contracts/interfaces/products/ISolaceCoverMinutes.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.6;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "./../utils/IGovernable.sol";
+
+/**
+ * @title Solace Cover Minutes
+ * @author solace.fi
+ * @notice
+ */
+interface ISolaceCoverMinutes is IERC20, IERC20Metadata, IGovernable {
+
+    event BalanceManagerStatusSet(address indexed balanceManager, bool status);
+    /*
+    mapping(address => bool) public isBalanceManager;
+    mapping(address => uint256) public balanceManagerIndex;
+    mapping(uint256 => address) public balanceManagers;
+    uint256 public balanceManagersLength;
+    */
+
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     */
+    function mint(address account, uint256 amount) external;
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function burn(address account, uint256 amount) external;
+
+    /**
+     * @dev Sets the balance of an account.
+     * @param account The account to change balance.
+     * @param amount The new balance of the account.
+     */
+    function setBalance(address account, uint256 amount) external;
+
+    /***************************************
+    GOVERNANCE FUNCTIONS
+    ***************************************/
+
+    /**
+     * @notice Adds or removes a set of balance managers.
+     * Can only be called by the current [**governor**](/docs/protocol/governance).
+     * @param bms List of balance managers to set.
+     * @param statuses Statuses to set.
+     */
+    function setBalanceManagerStatuses(address[] calldata bms, bool[] calldata statuses) external;
+}
---
+++ contracts/products/SolaceCoverMinutes.sol
@@ -0,0 +1,298 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.6;
+
+//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "./../utils/Governable.sol";
+//import "./interfaces/ISOLACE.sol";
+import "./../interfaces/products/ISolaceCoverMinutes.sol";
+
+/**
+ * @title Solace Cover Minutes
+ * @author solace.fi
+ * @notice
+ */
+contract SolaceCoverMinutes is Context, ISolaceCoverMinutes, Governable {
+
+    /***************************************
+    ERC20 DATA
+    ***************************************/
+
+    mapping(address => uint256) private _balances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+
+    /***************************************
+    BALANCE MANAGER DATA
+    ***************************************/
+
+    mapping(address => bool) public isBalanceManager;
+    mapping(address => uint256) public balanceManagerIndex;
+    mapping(uint256 => address) public balanceManagers;
+    uint256 public balanceManagersLength;
+
+    /***************************************
+    CONSTRUCTOR
+    ***************************************/
+
+    /**
+     * @notice Constructs the Solace Cover Minutes contract.
+     * @param governance_ The address of the [governor](/docs/protocol/governance).
+     */
+    constructor(address governance_) Governable(governance_) {
+        _name = "scm";
+        _symbol = "SCM";
+    }
+
+    /***************************************
+    ERC20 FUNCTIONS
+    ***************************************/
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the value {ERC20} uses, unless this function is
+     * overridden;
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view virtual override returns (uint8) {
+        return 18;
+    }
+
+    /**
+     * @dev See {IERC20-totalSupply}.
+     */
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev See {IERC20-balanceOf}.
+     */
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return _balances[account];
+    }
+
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `recipient` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+     */
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        require(isBalanceManager[msg.sender], "!balance manager");
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-transferFrom}.
+     *
+     * Emits an {Approval} event indicating the updated allowance. This is not
+     * required by the EIP. See the note at the beginning of {ERC20}.
+     *
+     * Requirements:
+     *
+     * - `sender` and `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     * - the caller must have allowance for ``sender``'s tokens of at least
+     * `amount`.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        require(isBalanceManager[msg.sender], "!balance manager");
+        _transfer(sender, recipient, amount);
+        return true;
+    }
+
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     */
+    function mint(address account, uint256 amount) external override {
+        require(isBalanceManager[msg.sender], "!balance manager");
+        _mint(account, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function burn(address account, uint256 amount) external override {
+        require(isBalanceManager[msg.sender], "!balance manager");
+        _burn(account, amount);
+    }
+
+    /**
+     * @dev Sets the balance of an account.
+     * @param account The account to change balance.
+     * @param amount The new balance of the account.
+     */
+    function setBalance(address account, uint256 amount) external override {
+        require(isBalanceManager[msg.sender], "!balance manager");
+        uint256 bal = _balances[account];
+        if(amount > bal) _mint(account, amount - bal);
+        else if(amount < bal) _burn(account, bal - amount);
+    }
+
+    /**
+     * @dev See {IERC20-allowance}.
+     */
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return 0;
+    }
+
+    /**
+     * @dev See {IERC20-approve}.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        revert("SCM: token not approvable");
+    }
+
+    /**
+     * @dev Moves `amount` of tokens from `sender` to `recipient`.
+     *
+     * This internal function is equivalent to {transfer}, and can be used to
+     * e.g. implement automatic token fees, slashing mechanisms, etc.
+     *
+     * Emits a {Transfer} event.
+     *
+     * Requirements:
+     *
+     * - `sender` cannot be the zero address.
+     * - `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     */
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal virtual {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        uint256 senderBalance = _balances[sender];
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        unchecked {
+            _balances[sender] = senderBalance - amount;
+        }
+        _balances[recipient] += amount;
+
+        emit Transfer(sender, recipient, amount);
+    }
+
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     */
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function _burn(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+        unchecked {
+            _balances[account] = accountBalance - amount;
+        }
+        _totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
+    }
+
+    /***************************************
+    GOVERNANCE FUNCTIONS
+    ***************************************/
+
+    /**
+     * @notice Adds or removes a set of balance managers.
+     * Can only be called by the current [**governor**](/docs/protocol/governance).
+     * @param bms List of balance managers to set.
+     * @param statuses Statuses to set.
+     */
+    function setBalanceManagerStatuses(address[] calldata bms, bool[] calldata statuses) external override onlyGovernance {
+        uint256 len = bms.length;
+        require(statuses.length == len, "length mismatch");
+        for(uint256 i = 0; i < len; i++) {
+            // if new balance manager
+            if(balanceManagerIndex[bms[i]] == 0) {
+                // add to enumeration
+                uint256 bmi = ++balanceManagersLength;
+                balanceManagerIndex[bms[i]] = bmi;
+                balanceManagers[bmi] = bms[i];
+            }
+            // set status
+            isBalanceManager[bms[i]] = statuses[i];
+            emit BalanceManagerStatusSet(bms[i], statuses[i]);
+        }
+    }
+}
