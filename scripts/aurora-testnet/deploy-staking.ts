46fdf7a26d273db182cc792a7d5c802b9f95a62a
Andrew Leonard <leonardishere@gmail.com>
2022-06-09 20:25:36 -0700

    standardize deploy staking

--- scripts/aurora-testnet/deploy-staking.ts
+++ scripts/aurora-testnet/deploy-staking.ts
@@ -4,17 +4,19 @@ import hardhat from "hardhat";
 const { waffle, ethers } = hardhat;
 const { provider } = waffle;
 const BN = ethers.BigNumber;
-import fs from "fs";
 import { config as dotenv_config } from "dotenv";
 dotenv_config();
-const deployer = new ethers.Wallet(JSON.parse(process.env.GOERLI_ACCOUNTS || '[]')[0], provider);
+const deployer = new ethers.Wallet(JSON.parse(process.env.PRIVATE_KEYS || '[]')[0], provider);
+
+import { create2ContractStashed } from "./../create2ContractStashed";
 
 import { logContractAddress } from "./../utils";
 
 import { import_artifacts, ArtifactImports } from "./../../test/utilities/artifact_importer";
-import { Deployer, Solace, XsLocker, XSolace, StakingRewards } from "../../typechain";
+import { Solace, XsLocker, XSolace, StakingRewards } from "../../typechain";
 import { expectDeployed, isDeployed } from "../../test/utilities/expectDeployed";
 import { getNetworkSettings } from "../getNetworkSettings";
+import { abiEncodeArgs } from "../../test/utilities/setStorage";
 
 const DEPLOYER_CONTRACT_ADDRESS     = "0x501acE4b4F9085348F60b61Fe3C95937a34565E7";
 const SOLACE_ADDRESS                = "0x501ACE0C6DeA16206bb2D120484a257B9F393891";
@@ -23,7 +25,6 @@ const STAKING_REWARDS_ADDRESS       = "0x501ACe4D89f596296C66f14D087a4BbB53Ed204
 const XSOLACE_ADDRESS               = "0x501ACEF0358fb055027A89AE46387a53C75498e0";
 
 let artifacts: ArtifactImports;
-let deployerContract: Deployer;
 let solace: Solace;
 let xslocker: XsLocker;
 let xsolace: XSolace;
@@ -41,7 +42,6 @@ async function main() {
   networkSettings = getNetworkSettings(chainID);
 
   await expectDeployed(DEPLOYER_CONTRACT_ADDRESS);
-  deployerContract = (await ethers.getContractAt(artifacts.Deployer.abi, DEPLOYER_CONTRACT_ADDRESS)) as Deployer;
   await deploySOLACE();
   await deployXSLocker();
   await deployStakingRewards();
@@ -55,9 +55,15 @@ async function deploySOLACE() {
     solace = (await ethers.getContractAt(artifacts.SOLACE.abi, SOLACE_ADDRESS)) as Solace;
   } else {
     console.log("Deploying SOLACE");
-    let bytecode = fs.readFileSync("scripts/contract_deploy_bytecodes/aurora_testnet/SOLACE.txt").toString().trim();
-    let tx = await deployer.sendTransaction({...networkSettings.overrides, to: DEPLOYER_CONTRACT_ADDRESS, gasLimit: 5000000, data: bytecode});
-    await tx.wait(networkSettings.confirmations);
+    await create2ContractStashed(
+      "SOLACE",
+      "scripts/contract_deploy_bytecodes_aurora_testnet/SOLACE.txt",
+      "stash/contracts_processed/SOLACE.sol",
+      deployer,
+      DEPLOYER_CONTRACT_ADDRESS,
+      SOLACE_ADDRESS,
+      abiEncodeArgs([signerAddress])
+    );
     solace = (await ethers.getContractAt(artifacts.SOLACE.abi, SOLACE_ADDRESS)) as Solace;
     console.log(`Deployed SOLACE to ${solace.address}`);
 
@@ -66,14 +72,6 @@ async function deploySOLACE() {
     await tx2.wait(networkSettings.confirmations);
     console.log("Added deployer as SOLACE minter");
   }
-  /*
-  if(await solace.governance() === signerAddress && await solace.pendingGovernance() !== multisigAddress) {
-    console.log(`solace.setPendingGovernance(${multisigAddress})`)
-    let tx = await solace.connect(deployer).setPendingGovernance(multisigAddress);
-    await tx.wait(networkSettings.confirmations);
-    console.log('set');
-  }
-  */
 }
 
 async function deployXSLocker() {
@@ -81,9 +79,15 @@ async function deployXSLocker() {
     xslocker = (await ethers.getContractAt(artifacts.xsLocker.abi, XSLOCKER_ADDRESS)) as unknown as XsLocker;
   } else {
     console.log("Deploying xsLocker");
-    let bytecode = fs.readFileSync("scripts/contract_deploy_bytecodes/aurora_testnet/staking/xsLocker.txt").toString().trim();
-    let tx = await deployer.sendTransaction({...networkSettings.overrides, to: DEPLOYER_CONTRACT_ADDRESS, gasLimit: 5000000, data: bytecode});
-    await tx.wait(networkSettings.confirmations);
+    await create2ContractStashed(
+      "xsLocker",
+      "scripts/contract_deploy_bytecodes_aurora_testnet/staking/xsLocker.txt",
+      "stash/contracts_processed/staking/xsLocker.sol",
+      deployer,
+      DEPLOYER_CONTRACT_ADDRESS,
+      XSLOCKER_ADDRESS,
+      abiEncodeArgs([signerAddress, SOLACE_ADDRESS])
+    );
     xslocker = (await ethers.getContractAt(artifacts.xsLocker.abi, XSLOCKER_ADDRESS)) as unknown as XsLocker;
     console.log(`Deployed xsLocker to ${xslocker.address}`);
   }
@@ -99,23 +103,29 @@ async function deployStakingRewards() {
     stakingRewards = (await ethers.getContractAt(artifacts.StakingRewards.abi, STAKING_REWARDS_ADDRESS)) as StakingRewards;
   } else {
     console.log("Deploying StakingRewards");
-    let bytecode = fs.readFileSync("scripts/contract_deploy_bytecodes/aurora_testnet/staking/StakingRewards.txt").toString().trim();
-    let tx = await deployer.sendTransaction({...networkSettings.overrides, to: DEPLOYER_CONTRACT_ADDRESS, gasLimit: 5000000, data: bytecode});
-    await tx.wait(networkSettings.confirmations);
+    await create2ContractStashed(
+      "StakingRewards",
+      "scripts/contract_deploy_bytecodes_aurora_testnet/staking/StakingRewards.txt",
+      "stash/contracts_processed/staking/StakingRewards.sol",
+      deployer,
+      DEPLOYER_CONTRACT_ADDRESS,
+      STAKING_REWARDS_ADDRESS,
+      abiEncodeArgs([signerAddress, SOLACE_ADDRESS, XSLOCKER_ADDRESS, solacePerSecond])
+    );
     stakingRewards = (await ethers.getContractAt(artifacts.StakingRewards.abi, STAKING_REWARDS_ADDRESS)) as StakingRewards;
     console.log(`Deployed StakingRewards to ${stakingRewards.address}`);
 
     console.log("staking rewards - registering in xslocker");
-    let tx1 = await xslocker.connect(deployer).addXsLockListener(stakingRewards.address);
+    let tx1 = await xslocker.connect(deployer).addXsLockListener(stakingRewards.address, networkSettings.overrides);
     await tx1.wait(networkSettings.confirmations);
     console.log("staking rewards - set rewards");
-    let tx2 = await solace.connect(deployer).mint(stakingRewards.address, solacePerYear);
+    let tx2 = await stakingRewards.connect(deployer).setRewards(solacePerSecond, networkSettings.overrides);
     await tx2.wait(networkSettings.confirmations);
     console.log("staking rewards - set times");
-    let tx3 = await stakingRewards.connect(deployer).setTimes(startTime, endTime);
+    let tx3 = await stakingRewards.connect(deployer).setTimes(startTime, endTime, networkSettings.overrides);
     await tx3.wait(networkSettings.confirmations);
     console.log("staking rewards - minting SOLACE");
-    let tx4 = await solace.connect(deployer).mint(stakingRewards.address, solacePerYear);
+    let tx4 = await solace.connect(deployer).mint(stakingRewards.address, solacePerYear, networkSettings.overrides);
     await tx4.wait(networkSettings.confirmations);
   }
 }
@@ -125,9 +135,15 @@ async function deployXSOLACE() {
     xsolace = (await ethers.getContractAt(artifacts.xSOLACE.abi, XSOLACE_ADDRESS)) as XSolace;
   } else {
     console.log("Deploying xSOLACE");
-    let bytecode = fs.readFileSync("scripts/contract_deploy_bytecodes/aurora_testnet/staking/xSOLACE.txt").toString().trim();
-    let tx = await deployer.sendTransaction({...networkSettings.overrides, to: DEPLOYER_CONTRACT_ADDRESS, gasLimit: 5000000, data: bytecode});
-    await tx.wait(networkSettings.confirmations);
+    await create2ContractStashed(
+      "xSOLACE",
+      "scripts/contract_deploy_bytecodes_aurora_testnet/staking/xSOLACE.txt",
+      "stash/contracts_processed/staking/xSOLACE.sol",
+      deployer,
+      DEPLOYER_CONTRACT_ADDRESS,
+      XSOLACE_ADDRESS,
+      abiEncodeArgs([XSLOCKER_ADDRESS])
+    );
     xsolace = (await ethers.getContractAt(artifacts.xSOLACE.abi, XSOLACE_ADDRESS)) as XSolace;
     console.log(`Deployed xSOLACE to ${xsolace.address}`);
   }
