2c5aace9f3a77a6768c6ef6295d90177db8428ec
kyzooghost <jeffersonzjtang@gmail.com>
2022-07-06 06:37:20 +1000

    removed governance from xslockerextension deploy script constructors, successful deploy on mumbai

--- scripts/aurora/deploy-xslockerextension.ts
+++ scripts/aurora/deploy-xslockerextension.ts
@@ -33,7 +33,6 @@ import { create2Contract } from "../create2Contract";
 const DEPLOYER_CONTRACT_ADDRESS  = "0x501aCe4732E4A80CC1bc5cd081BEe7f88ff694EF";
 const SOLACE_ADDRESS             = "0x501acE9c35E60f03A2af4d484f49F9B1EFde9f40";
 const XSLOCKER_ADDRESS           = "0x501Ace47c5b0C2099C4464f681c3fa2ECD3146C1";
-const GOVERNANCE_ADDRESS         = ""
 const XSLOCKEREXTENSION_ADDRESS  = ""
 const MAX_UINT256 = BN.from("115792089237316195423570985008687907853269984665640564039457584007913129639935")
 
@@ -72,7 +71,7 @@ async function deployXsLockerExtension() {
       xsLockerExtension = (await ethers.getContractAt(artifacts.xsLockerExtension.abi, XSLOCKEREXTENSION_ADDRESS)) as XsLockerExtension;
     } else {
       console.log("Deploying XsLockerExtension");
-      const res = await create2Contract(deployer, artifacts.xsLockerExtension, [GOVERNANCE_ADDRESS, SOLACE_ADDRESS, XSLOCKER_ADDRESS], {}, "", DEPLOYER_CONTRACT_ADDRESS);
+      const res = await create2Contract(deployer, artifacts.xsLockerExtension, [SOLACE_ADDRESS, XSLOCKER_ADDRESS], {}, "", DEPLOYER_CONTRACT_ADDRESS);
       xsLockerExtension = (await ethers.getContractAt(artifacts.xsLockerExtension.abi, res.address)) as unknown as XsLockerExtension;
       console.log(`Deployed XsLockerExtension to ${xsLockerExtension.address}`);
     }
